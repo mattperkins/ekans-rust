326ae768cf8810fd5afcb66871ec87fbf56e09a5
mattperkins <github@mattperkins.com>
2019-03-23 18:30:03 +0000

    draw green screen and render to window

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,44 @@ extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
 
-use glutin_window::GlutinWindow as Window;
+use glutin_window::GlutinWindow;
 use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::*;
 use piston::input::*;
 use piston::window::WindowSettings;
 
+struct Game {
+    gl: GlGraphics,
+}
+impl Game {
+    fn render(&mut self, arg: &RenderArgs) {
+        use graphics;
+
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+
+        self.gl.draw(arg.viewport(), |_c, gl| {
+            graphics::clear(GREEN, gl);
+        });
+    }
+}
+
 fn main() {
-    println!("preach...");
+    let opengl = OpenGL::V3_2;
+
+    let mut window: GlutinWindow = WindowSettings::new("Ekans", [200, 200])
+        .opengl(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut game = Game {
+        gl: GlGraphics::new(opengl),
+    };
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(r) = e.render_args() {
+            game.render(&r);
+        }
+    }
 }
