673f6bd4c0024c1d0f72221047ad81bb4b4ba586
mattperkins <github@mattperkins.com>
2019-03-23 18:09:37 +0000

    use deps

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@ extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
 
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::*;
+use piston::input::*;
+use piston::window::WindowSettings;
+
 fn main() {
     println!("preach...");
 }
